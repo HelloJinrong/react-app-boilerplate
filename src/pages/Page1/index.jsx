fb5d7e71e13c1836514e553d3e7d062cdef6fd3e
wei_gao <ACI_weigao@gaoweideMacBook-Pro.local>
2020-12-01 10:26:35 +0800

    feat: add eslint for project

--- src/pages/Page1/index.jsx
+++ src/pages/Page1/index.jsx
@@ -5,27 +5,27 @@ import { numState } from 'stores/Page1Store';
 import './index.scss';
 
 const Page1 = () => {
-  const history = useHistory();
-  const [num, setNum] = useRecoilState(numState);
+	const history = useHistory();
+	const [num, setNum] = useRecoilState(numState);
 
-  const incNum = () => setNum(num + 1);
+	const incNum = () => setNum(num + 1);
 
-  const decNum = () => setNum(num - 1);
+	const decNum = () => setNum(num - 1);
 
-  const handleToPage = () => history.push('/page2');
+	const handleToPage = () => history.push('/page2');
 
-  return (
-    <div className="page1">
-      this is Page1 component!
-      <p>{num}</p>
-      <div>
-        <button onClick={incNum}>++</button>
-        <button onClick={decNum}>- -</button>
+	return (
+		<div className="page1">
+			this is Page1 component!
+			<p>{num}</p>
+			<div>
+				<button onClick={incNum}>++</button>
+				<button onClick={decNum}>- -</button>
 
-        <button onClick={handleToPage}>to Page2</button>
-      </div>
-    </div>
-  );
+				<button onClick={handleToPage}>to Page2</button>
+			</div>
+		</div>
+	);
 };
 
 export default Page1;
--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -1,32 +1,37 @@
 const path = require('path');
 const WebpackBar = require('webpackbar');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const { merge } = require('webpack-merge');
 
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const baseConf = require('./config');
 
-const resolve = (dir) => path.resolve(__dirname, '..', dir);
+const resolve = dir => path.resolve(__dirname, '..', dir);
 
 const config = {
-  entry: ['./src/index.js'],
-  devtool: 'eval-cheap-module-source-map',
-  performance: {
-    hints: false,
-  },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    port: 3001,
-    quiet: true,
-    publicPath: '/',
-    compress: true,
-  },
-  plugins: [
-    new WebpackBar({
-      name: 'development',
-    }),
-    new FaviconsWebpackPlugin(resolve('public/favicon.ico')),
-  ],
+	entry: ['./src/index.js'],
+	devtool: 'eval-cheap-module-source-map',
+	performance: {
+		hints: false,
+	},
+	devServer: {
+		historyApiFallback: true,
+		hot: true,
+		port: 3001,
+		quiet: true,
+		publicPath: '/',
+		compress: true,
+		overlay: true,
+	},
+	plugins: [
+		new WebpackBar({
+			name: 'development',
+		}),
+		new FaviconsWebpackPlugin(resolve('public/favicon.ico')),
+		new ESLintPlugin({
+			extensions: ['.js', '.jsx'],
+		}),
+	],
 };
 
 module.exports = merge(baseConf, config);
