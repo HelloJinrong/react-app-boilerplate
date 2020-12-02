7d743c3c085e86efbfc4182f9b0de0a73ad1c816
qinghuanI <1062217965@qq.com>
2020-12-02 21:13:00 +0800

    chore: whether the current port is occupied, and return to the new port if occupied

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -3,12 +3,19 @@ const WebpackBar = require('webpackbar');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const { merge } = require('webpack-merge');
+const portfinder = require('portfinder');
 
 const baseConf = require('./config');
 
 const resolve = dir => path.resolve(__dirname, '..', dir);
 
-const config = {
+const { PORT } = process.env;
+
+if (PORT) {
+	portfinder.basePort = PORT;
+}
+
+const config = merge(baseConf, {
 	entry: ['./src/index.js'],
 	devtool: 'eval-cheap-module-source-map',
 	performance: {
@@ -17,11 +24,11 @@ const config = {
 	devServer: {
 		historyApiFallback: true,
 		hot: true,
-		port: 3001,
 		quiet: true,
 		publicPath: '/',
 		compress: true,
 		overlay: true,
+		stats: 'errors-only',
 	},
 	plugins: [
 		new WebpackBar({
@@ -32,6 +39,17 @@ const config = {
 			extensions: ['.js', '.jsx'],
 		}),
 	],
-};
+});
 
-module.exports = merge(baseConf, config);
+module.exports = new Promise((resolve, reject) => {
+	portfinder.getPort((err, port) => {
+		if (err) {
+			reject(err);
+			return;
+		}
+
+		config.devServer.port = port;
+
+		resolve(config);
+	});
+});
