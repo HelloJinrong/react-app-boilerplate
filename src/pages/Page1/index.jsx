071f9faf9a23b0cd15e90cd3fedf79759aea90c0
wei_gao <ACI_weigao@gaoweideMacBook-Pro.local>
2020-12-09 12:31:38 +0800

    chore: remove useless files

--- src/pages/Page1/index.jsx
+++ src/pages/Page1/index.jsx
@@ -1,23 +1,38 @@
+import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { getCurrentTime } from 'utils/datetime';
 
 import './index.scss';
 
-import dayjs from 'dayjs';
-// eslint-disable-next-line no-console
-console.log('time =', dayjs('2003-1-4').format('MM-DD-YYYY'));
+const set = new Set();
 
 const Page1 = () => {
 	const history = useHistory();
 
 	const handleToPage = () => history.push('/page2');
 
+	const handleClick = () => {
+		// eslint-disable-next-line no-console
+		console.log(111);
+	};
+
+	const handleClick2 = () => {
+		// eslint-disable-next-line no-console
+		console.log(222);
+	};
+
+	useEffect(() => {
+		set.add(handleClick2);
+		// eslint-disable-next-line no-console
+		console.log('length =', set.size);
+	}, []);
+
 	return (
 		<div className="page1">
 			this is Page1 component!
 			<div>
 				<button onClick={handleToPage}>to Page2</button>
-				<button>点击</button>
+				<button onClick={handleClick}>点击</button>
+				<button onClick={handleClick2}>点击2</button>
 			</div>
 		</div>
 	);
