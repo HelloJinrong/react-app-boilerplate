bf2c82d172a2a0112ed112d8d5522fdb30126dce
qinghuanI <1062217965@qq.com>
2020-12-27 11:36:45 +0800

    chore: add some useHooks functions

--- src/pages/Page2/index.jsx
+++ src/pages/Page2/index.jsx
@@ -1,15 +1,33 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+/* eslint-disable no-debugger */
+/* eslint-disable no-unused-vars */
 import { useHistory } from 'react-router-dom';
 
+import useImageSize from 'components/useImageSize';
 import useInputValue from 'components/useInputValue';
 
 const Page2 = () => {
+	const imageUrl = 'https://pic.feizl.com/upload/allimg/170614/235253EG-2.jpg';
 	const history = useHistory();
 	const [value, onChange] = useInputValue('qin');
+
+	const url = 'https://cdn.int64ago.org/ogk39i54.png';
+
+	const [width, height] = useImageSize(imageUrl);
+
+	console.log('w, h =', width, height);
 	const handleToPage = () => history.push('/page1');
 
 	const handleGetValue = () => {
 		// eslint-disable-next-line no-console
 		console.log('value =', value);
+
+		// setFirstName('qin');
+	};
+
+	const handleRemoveCookie = () => {
+		// removeFirstName('name');
 	};
 
 	return (
@@ -18,6 +36,7 @@ const Page2 = () => {
 			<input value={value} onChange={onChange}></input>
 			<button onClick={handleToPage}>to Page1</button>
 			<button onClick={handleGetValue}>获取输入框的值</button>
+			<button onClick={handleRemoveCookie}>移除cookie</button>
 		</div>
 	);
 };
