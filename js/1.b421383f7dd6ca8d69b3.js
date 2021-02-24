8f56609d9f994d36cccf20fde4bcfc26b82e5a28
qinghuanI <45612221+qinghuanI@users.noreply.github.com>
2021-02-24 06:09:42 +0000

    Deploying to gh-pages from @ qinghuanI/react-app-boilerplate@bb26c8952f1f1da16e53d8bc43de3b7a2b24a7f1 🚀

---
+++ js/1.b421383f7dd6ca8d69b3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1,4],{14:function(a,t,e){"use strict";e.r(t);var n=e(1);e(60),t.default=()=>{const c=Object(n.g)();return React.createElement("div",{className:"page2"},"this is Page2 component!",React.createElement("button",{onClick:()=>c.push("/page1")},"to Page1"))}},60:function(a,t,e){}}]);
--- js/3.f2c5360aabc6250a9eb1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{74:function(n,w,o){}}]);
